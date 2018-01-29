8959848f4079f44eba81c23612efc0e9145a9673
Patryk <rastawafel03@gmail.com>
2018-01-29 14:59:15 +0100

    Added public to Main method because of 100 lines of errors

---
+++ src/sample/Checking.java
@@ -0,0 +1,48 @@
+package sample;
+
+import javafx.scene.control.TextField;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Checking {
+
+    public boolean LuhnAlgorithm(String CardNumbNoSpace) {
+        int sum = 0;
+        boolean alternate = false;
+        for (int i = CardNumbNoSpace.length() - 1; i >= 0; i--) {
+            int n = Integer.parseInt(CardNumbNoSpace.substring(i, i + 1));
+            if (alternate) {
+                n *= 2;
+                if (n > 9) {
+                    n = (n % 10) + 1;
+                }
+            }
+            sum += n;
+            alternate = !alternate;
+        }
+        return (sum % 10 == 0);
+    }
+
+    public boolean DayMonthInput(int dayBox, int monthBox) {
+        return dayBox <= 31 && monthBox <= 12;
+    }
+
+    public void ZerosInDate(TextField DDinput, TextField MMinput,
+                               TextField YYYYinput, DateFormat df) {
+        try {
+            Date InputDate;
+            if (DDinput.getText().length() == 1) {
+                InputDate = df.parse('0' + DDinput.getText() + MMinput.getText() + YYYYinput.getText());
+            } else if (MMinput.getText().length() == 1) {
+                InputDate = df.parse(DDinput.getText() + '0' + MMinput.getText() + YYYYinput.getText());
+            } else if (DDinput.getText().length() == 1 && MMinput.getText().length() == 1) {
+                InputDate = df.parse('0' + DDinput.getText() + '0' + MMinput.getText() + YYYYinput.getText());
+            }
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+
+    }
+
+}
---
+++ src/sample/CreditCardChecker.java
@@ -0,0 +1,114 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CreditCardChecker extends Application {
+
+
+    Stage window;
+
+    @Override
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+        Button btn = new Button();
+        GridPane grid = new GridPane();
+
+        grid.setVgap(5);
+        grid.setHgap(5);
+
+        grid.setPadding(new Insets(10, 10, 10, 10));
+
+
+        TextField CardNumInput = new TextField();
+        CardNumInput.setPromptText("Card Number");
+        GridPane.setConstraints(CardNumInput, 0, 0);
+
+        TextField CCVInput = new TextField();
+        CCVInput.setPromptText("CCV");
+        CCVInput.setPrefWidth(40);
+        GridPane.setConstraints(CCVInput, 1, 0);
+
+
+        TextField DDinput = new TextField();
+        DDinput.setPromptText("DD");
+
+        DDinput.setMaxWidth(37);
+        GridPane.setConstraints(DDinput, 4, 0);
+
+
+        TextField MMinput = new TextField();
+        MMinput.setPromptText("MM");
+        MMinput.setMaxWidth(37);
+        GridPane.setConstraints(MMinput, 5, 0);
+
+
+        TextField YYYYinput = new TextField();
+        YYYYinput.setPromptText("YYYY");
+        YYYYinput.setMaxWidth(43);
+        GridPane.setConstraints(YYYYinput, 6, 0);
+
+        TextField CorrectOrNot = new TextField();
+        GridPane.setConstraints(CorrectOrNot, 0, 7);
+
+        GridPane.setConstraints(btn, 0, 4);
+        btn.setMaxWidth(300);
+
+        btn.setText("Check your credit card!");
+        btn.setOnAction(e
+                -> isNumber(CardNumInput, CCVInput, DDinput, MMinput, YYYYinput, CardNumInput.getText(), CorrectOrNot));
+
+        grid.getChildren().addAll(CardNumInput, CCVInput, DDinput, MMinput, YYYYinput, btn, CorrectOrNot);
+
+        Scene scene = new Scene(grid, 350, 150);
+        window.setScene(scene);
+
+        window.show();
+    }
+
+    public boolean isNumber(TextField number, TextField CCV, TextField DDinput, TextField MMinput,
+                            TextField YYYYinput, String MSG, TextField CorrectOrNot) {
+
+        String cardNumb = number.getText();
+        String cardNumbWithoutSpaces = cardNumb.replaceAll("\\s", "");
+        int CCVLenght = CCV.getLength();
+        int dayBox = Integer.parseInt(DDinput.getText());
+        int monthBox = Integer.parseInt(MMinput.getText());
+
+        DateFormat df = new SimpleDateFormat("ddMMyyyy");
+        Date todaysDate = new Date();
+        Checking checking = new Checking();
+
+        try {
+            Date inputDate = df.parse(DDinput.getText() + MMinput.getText() + YYYYinput.getText());
+
+            checking.ZerosInDate(DDinput, MMinput,YYYYinput, df);
+
+            if ("^[\\d]{12,19}$".matches(cardNumbWithoutSpaces) && (CCVLenght == 3) && todaysDate.before(inputDate) &&
+                    checking.DayMonthInput(dayBox, monthBox) && checking.LuhnAlgorithm(cardNumbWithoutSpaces))
+            {
+                CorrectOrNot.setText("Card is valid");
+            } else {
+                CorrectOrNot.setText("Error");
+            }
+            return true;
+
+        } catch (Exception e) {
+            CorrectOrNot.setText("Error");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
